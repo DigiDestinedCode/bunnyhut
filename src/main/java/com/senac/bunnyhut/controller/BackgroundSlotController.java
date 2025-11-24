0b380032f968bd786b2664b4ff9a5dc310f26195
Igor Ismael <igorismel@gmail.com>
2025-11-24 19:19:04 -0300

    Tentando concertar nomes

--- src/main/java/com/senac/bunnyhut/controller/BackgroundSlotController.java
+++ src/main/java/com/senac/bunnyhut/controller/BackgroundSlotController.java
@@ -1,9 +1,8 @@
 package com.senac.bunnyhut.controller;
 
-import com.senac.bunnyhut.dto.request.Background_SlotDTORequest;
-import com.senac.bunnyhut.dto.response.Background_SlotDTOResponse;
-import com.senac.bunnyhut.dto.response.Background_SlotDTOUpdateResponse;
-import com.senac.bunnyhut.service.Background_SlotService;
+import com.senac.bunnyhut.dto.request.BackgroundSlotDTORequest;
+import com.senac.bunnyhut.dto.response.BackgroundSlotDTOResponse;
+import com.senac.bunnyhut.service.BackgroundSlotService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -16,9 +15,9 @@ import java.util.List;
 @RequestMapping("api/background-slot")
 public class Background_SlotController {
 
-    private final Background_SlotService background_slotService;
+    private final BackgroundSlotService background_slotService;
 
-    public Background_SlotController(Background_SlotService background_slotService) {
+    public Background_SlotController(BackgroundSlotService background_slotService) {
         this.background_slotService = background_slotService;
     }
 
@@ -27,7 +26,7 @@ public class Background_SlotController {
             summary = "Listar background_slots",
             description = "Endpoint para listar todos os background_slots"
     )
-    public ResponseEntity<List<Background_SlotDTOResponse>> listBackground_Slots() {
+    public ResponseEntity<List<BackgroundSlotDTOResponse>> listBackground_Slots() {
         return ResponseEntity.ok(background_slotService.listBackground_Slots());
     }
 
@@ -36,8 +35,8 @@ public class Background_SlotController {
             summary = "Listar background_slot pelo id de background_slot",
             description = "Endpoint para listar background_slot por Id de background_slot"
     )
-    public ResponseEntity<Background_SlotDTOResponse> listarPorBackground_SlotId(@PathVariable("background_slotId") Integer background_slotId) {
-        Background_SlotDTOResponse background_slot = background_slotService.listarPorBackground_SlotId(background_slotId);
+    public ResponseEntity<BackgroundSlotDTOResponse> listarPorBackground_SlotId(@PathVariable("background_slotId") Integer background_slotId) {
+        BackgroundSlotDTOResponse background_slot = background_slotService.listarPorBackground_SlotId(background_slotId);
         if (background_slot == null) {
             return ResponseEntity.noContent().build();
         } else {
@@ -49,8 +48,8 @@ public class Background_SlotController {
             summary = "Criar novo background_slot",
             description = "Endpoint para criar um novo registro de background_slot"
     )
-    public ResponseEntity<Background_SlotDTOResponse> criarBackground_Slot(
-            @Valid @RequestBody Background_SlotDTORequest background_slot
+    public ResponseEntity<BackgroundSlotDTOResponse> criarBackground_Slot(
+            @Valid @RequestBody BackgroundSlotDTORequest background_slot
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(background_slotService.criarBackground_Slot(background_slot));
     }
@@ -60,9 +59,9 @@ public class Background_SlotController {
             summary = "Atualizar todos os dados do background_slot",
             description = "Endpoint para atualizar o registro de background_slot"
     )
-    public ResponseEntity<Background_SlotDTOResponse> atualizarBackground_Slot(
+    public ResponseEntity<BackgroundSlotDTOResponse> atualizarBackground_Slot(
             @PathVariable("background_slotId") Integer background_slotId,
-            @Valid @RequestBody Background_SlotDTORequest background_slotDTORequest
+            @Valid @RequestBody BackgroundSlotDTORequest background_slotDTORequest
     ) {
         return ResponseEntity.ok(background_slotService.atualizarBackground_Slot(background_slotId, background_slotDTORequest));
     }
